459eb79287f79697ec1cd2dcb5f248c724859e7e
rahulrksmau <rkvns15@gmail.com>
2017-09-26 00:05:48 +0530

    Updated with static member and static function

--- module#11.cpp
+++ module#11.cpp
@@ -4,12 +4,23 @@ using namespace std;
 /*
  Static data member
  Static data function
+ It can be public and private.
+ Is Shared by all object of the class.
+ static function have no this pointer (not associated with any object)
+ static function cannot access not static function and members of the class.
+ cannot coexist with non static function
+ cannot const
+
  Associated with class not objects hence call by scope resolution(::) not by dot(.). And also must be initialize and declare explicitly out of class.
  static function cannot contain class data member.
 */
 class Clock{
     static int hour;
 public:
+    Clock(int n){
+        hour += n;
+        cout << "Constructor called ";
+    }
     void time(){
         hour = 1;
     }
@@ -19,8 +30,31 @@ public:
         cout << "Time is = " << hour << endl;
     }
 };
-int Clock::hour=0;
+int Clock::hour=0; // initialize and declare
 
+class Process{
+    int page;
+    static int nPages;
+    static int nJobs;
+public:
+    Process(int n): page(n){
+        nJobs += 1;
+        nPages -= n;
+        cout << "Process Start total process = "<<job <<endl ;
+    }
+    ~Process(){
+        nJobs -= 1;
+        cout << "Process end \n";
+    }
+    static int getJobs(){ return nJobs;}
+    static int checkPages(){ return nPages; }
+    static void loadPages(int n){
+        nPages += n;
+    }
+
+};
+int Process::nPages = 1000;
+int Process::nJobs = 0;
 int main(){
     Clock* C = new Clock();
     Clock c1,c2;
@@ -33,4 +67,8 @@ int main(){
        // c2.time();
         c2.click();
     }
+    Process p1,p2; // only process created by this type call destructor
+    Process* p3 = new Process();
+    cout << p1.getJobs() << endl;
+    cout << p1.checkPages() << endl;
 }
